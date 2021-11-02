18c8b32f944200b0070e7b2c582a5aef777faddd
Krystian Stopka <kr6stopka@gmail.com>
2021-11-02 18:54:04 +0100

    form training

--- src/Nauka_react/5_form.tsx
+++ src/Nauka_react/5_form.tsx
@@ -1,3 +1,4 @@
+import e from "cors";
 import * as React from "react";
 
 interface AppProps {}
@@ -5,11 +6,61 @@ interface AppProps {}
 interface AppState {}
 
 class App extends React.Component<AppProps, AppState> {
-    // state = { :  }
+    state = { city: "Warszawa", text: "some Text", isLoved: true, number: "0" };
+    handleChange = (e: any) => {
+        if (e.target.type === "checkbox") {
+            this.setState({ [e.target.name]: e.target.checked });
+            return;
+        }
+        this.setState({ [e.target.name]: e.target.value });
+    };
     render() {
         return (
             <div className="wrapper">
                 <h1>Form</h1>
+                <div>
+                    <label>
+                        Podaj miasto:
+                        <div>
+                            <input name="city" type="text" value={this.state.city} onChange={this.handleChange} />
+                        </div>
+                    </label>
+                </div>
+                <div>
+                    <label htmlFor="">
+                        Proszę napisz coś ciekawego:
+                        <div>
+                            <textarea name="text" value={this.state.text} onChange={this.handleChange}></textarea>
+                        </div>
+                    </label>
+                </div>
+                <div>
+                    <label htmlFor="">
+                        Czy lubisz to miasto?
+                        <div>
+                            <input
+                                name="isLoved"
+                                type="checkbox"
+                                checked={this.state.isLoved}
+                                onChange={this.handleChange}
+                            />
+                        </div>
+                    </label>
+                </div>
+                <div>
+                    <label htmlFor="">
+                        Ile razy tam byles
+                        <div>
+                            <select name="number" value={this.state.number} onChange={this.handleChange.bind(this)}>
+                                <option value="0">0</option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="more">więcej</option>
+                            </select>
+                        </div>
+                    </label>
+                </div>
             </div>
         );
     }
