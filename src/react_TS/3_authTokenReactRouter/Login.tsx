ecb64d7518960b66f111307de01e35df1a4240a0
Krystian Stopka <kr6stopka@gmail.com>
2021-11-05 13:44:22 +0100

    przed CR

--- src/react_TS/3_authTokenReactRouter/Login.tsx
+++ src/react_TS/3_authTokenReactRouter/Login.tsx
@@ -7,6 +7,19 @@ const Login: FunctionComponent<LoginProps> = () => {
     return (
         <div>
             <h1>Login</h1>
+            <form>
+                <label>
+                    <p>Username</p>
+                    <input type="text" />
+                </label>
+                <label>
+                    <p>Password</p>
+                    <input type="password" />
+                </label>
+                <div>
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
         </div>
     );
 };
