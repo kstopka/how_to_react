5d4cfb4237dd53146d09c3e06cd9c5d06844bba1
Krystian Stopka <kr6stopka@gmail.com>
2021-11-11 22:14:32 +0100

    coś tam sie zrobiło

--- src/react_TS/0_newTask/NewTask.tsx
+++ src/react_TS/0_newTask/NewTask.tsx
@@ -1,9 +1,33 @@
 import * as React from "react";
 import { useState, useEffect, FunctionComponent } from "react";
 
+import dataRatings from "./data.json";
+
+type HelpRatingType = {
+    ratings: RatingType[];
+};
+
+export type RatingType = {
+    recordId: string;
+    name: string;
+    score: number;
+    content: string;
+};
+
 interface AppProps {}
 
 const App: FunctionComponent<AppProps> = () => {
+    const [ratings, setRatings] = useState([]);
+
+    useEffect(() => {
+        //NOTE: ... no nie działa cos :(
+        // try {
+        const data: RatingType[] = JSON.parse(dataRatings);
+        setRatings(data);
+        // } catch (error) {
+        // throw new Error(`Error: ${error}`);
+        // }
+    }, []);
     return (
         <div className="wrapper">
             <h1>Rating Stars</h1>
