0b79b567350b5395ad424c1ee4ed82a91287e738
Krystian Stopka <kr6stopka@gmail.com>
2021-11-18 13:03:34 +0100

    useMemoState

--- src/react_TS/2_useMemoStateHook/useMemoStateHook.tsx
+++ src/react_TS/2_useMemoStateHook/useMemoStateHook.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { FunctionComponent } from "react";
+import { FunctionComponent, useRef } from "react";
 
 interface AppProps {}
 
@@ -19,63 +19,42 @@ interface AppProps {}
 
 // react hooks
 const useMemoState = (initialValue: any) => {
-    let state: any = null;
-    let stateInitialized: boolean = false;
-    const setState = (newState: any) => {
-        console.log("use set state");
-        state = newState;
-        return state;
+    // let value = useRef(initialValue); // zapis do pamięci
+    // const prevValue = useRef()
+
+    let value = { current: [initialValue] }; // zapis do pamięci
+    const prevValue = { current: [null] };
+
+    const setter = (initialValue: any) => {
+        if (!checkArray([initialValue], prevValue.current)) {
+            value.current = initialValue;
+            prevValue.current = [...initialValue];
+        }
+        return [value, setter];
     };
 
-    if (!stateInitialized) {
-        stateInitialized = true;
-        state = initialValue;
-    }
-    return [state, setState];
+    return [value, setter];
+};
+
+const checkArray = (array: any[], value: any) => {
+    return array.includes(value);
 };
 
 const App: FunctionComponent<AppProps> = () => {
     const [value, setter] = useMemoState({ val: 0 });
     console.log(value);
-    console.log(setter);
 
-    setter({ value: 5 });
+    // setter({ value: 5 });
+    console.log(value);
+    // setter({ value: 6 });
     console.log(value);
 
     return (
         <div className="wrapper">
-            <h1>Use Memo State Hook</h1>
+            <p>Use Memo State Hook</p>
             {/* <div>{value}</div> */}
         </div>
     );
 };
 
 export default App;
-
-// var ReactCurrentDispatcher = {
-//     /**
-//      * @internal
-//      * @type {ReactComponent}
-//      */
-//     current: null,
-// };
-
-// function resolveDispatcher() {
-//     var dispatcher = ReactCurrentDispatcher.current;
-
-//     if (!(dispatcher !== null)) {
-//         // eslint-disable-next-line no-lone-blocks
-//         {
-//             throw Error(
-//                 "Invalid hook call. Hooks can only be called inside of the body of a function component. This could happen for one of the following reasons:\n1. You might have mismatching versions of React and the renderer (such as React DOM)\n2. You might be breaking the Rules of Hooks\n3. You might have more than one copy of React in the same app\nSee https://reactjs.org/link/invalid-hook-call for tips about how to debug and fix this problem."
-//             );
-//         }
-//     }
-
-//     return dispatcher;
-// }
-
-// function useState(initialState: number) {
-//     var dispatcher = resolveDispatcher();
-//     return dispatcher.useState(initialState);
-// }
