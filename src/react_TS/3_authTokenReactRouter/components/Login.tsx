48882c02000352fe3ade04f146d37b715fd807ee
Krystian Stopka <kr6stopka@gmail.com>
2021-11-24 14:35:17 +0100

    context ...

--- src/react_TS/3_authTokenReactRouter/components/Login.tsx
+++ src/react_TS/3_authTokenReactRouter/components/Login.tsx
@@ -1,34 +1,57 @@
 import * as React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Validator from "../Validator";
-import { AuthContextProvider } from "../authContext";
 
 // interface LoginProps {}
 //NOTE: podczas loginu ma przejsc na home
 //NOTE: jaki powienin być typ setToken
 const Login = ({ setToken }: { setToken: any }) => {
-    const [userName, setUserName] = useState("");
-    const [password, setPassword] = useState("");
+    // const [userName, setUserName] = useState("");
+    // const [password, setPassword] = useState("");
+
+    const [userNameField, setuserNameField] = useState({
+        login: "",
+        error: true,
+    });
 
     const handleChangeLogin = (e: any) => {
-        const userName = e.target.value;
-        // const error = Validator.throwErrorOnInvalidProperName(login, "error msg");
-        setUserName(userName);
+        const login = e.target.value;
+        const error = Validator.throwErrorOnInvalidProperName(login, "error msg");
+        setuserNameField({ login, error });
     };
-    const handleChangePassword = (e: any) => {
-        const password = e.target.value;
-        setPassword(password);
-    };
-    useEffect(() => {
-        AuthContextProvider(userName, password);
-    }, []);
+    // const handleChangePassword = (e: any) => {
+    //     //validacja
+    //     const password = e.target.value;
+    //     setPassword(password);
+    // };
+    // const handleFormSubmit = (e: { preventDefault: () => void }) => {
+    //     e.preventDefault();
+    //     // if (userName !== correctUserName || password !== correctPassword) {
+    //     // alert("wrong login or passowrd");
+    //     // }
+    //     setToken(true);
+    // };
 
+    //NOTE: jeżeli jest zalogowany to wrzuca na home
+    //NOTE: zmienia token na ten poprawny
+    //control inputs
+    //uncontrol
+
+    // const onSubmit = useCallback(()=>{
+    //     if(...){
+    //         return false
+    //     }
+
+    //     ...
+    // },[])
+
+    const error = "testowy error";
     return (
         <div>
             <h1>Login</h1>
             {/* <form onSubmit={handleSubmit}></form> */}
             <label htmlFor="">
-                {/* <small style={{ color: "red" }}>{error}</small> */}
+                <small style={{ color: "red" }}>{error}</small>
                 <p>User Login:</p>
                 <input type="text" onChange={handleChangeLogin} />
             </label>
--- src/react_TS/3_authTokenReactRouter/hooks/useCredentialsContext.tsx
+++ src/react_TS/3_authTokenReactRouter/hooks/useCredentialsContext.tsx
@@ -4,9 +4,9 @@ import { createContext, useContext } from "react";
 import { useCredentialsFromApi } from "../App.hooks";
 
 export const useCredentialsContext = () => {
-    const { credentials } = useCredentialsFromApi();
-    // const CredentialsContext = createContext(users);
-    // const credentails = useContext(CredentialsContext);
-    // //akcja:
-    // return credentails;
+    const { users } = useCredentialsFromApi();
+    const CredentialsContext = createContext(users);
+    const credentails = useContext(CredentialsContext);
+    //akcja:
+    return credentails;
 };
