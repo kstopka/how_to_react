04ac7fc9d708d74299bb906fd90a89fc52683720
Krystian Stopka <kr6stopka@gmail.com>
2021-12-24 10:18:57 +0100

    useCreateShuffledArrayWithInputs and filledArrayWithPassword

--- src/react_TS/9_PasswordInput/components/PasswordInput.tsx
+++ src/react_TS/9_PasswordInput/components/PasswordInput.tsx
@@ -1,58 +1,25 @@
 import * as React from "react";
 import { FunctionComponent } from "react";
 import "../css/style.css";
+import { useCreateShuffledArrayWithInputs } from "../hooks/useCreateShuffledArrayWithInputs";
 
 interface PasswordInputProps {
     password: string;
     onSuccess?: boolean;
 }
 
-const createArray = (number: number): number => {
-    const minNsmbers = Math.ceil(number * 1.5);
-    const maxNumbers = Math.ceil(number * 2);
-    return Math.floor(Math.random() * (maxNumbers + 1 - minNsmbers) + minNsmbers);
-};
-
-const shuffle = (array: any[]) => {
-    let currentIndex = array.length,
-        randomIndex;
-
-    // While there remain elements to shuffle...
-    while (currentIndex != 0) {
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-
-        // And swap it with the current element.
-        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
-    }
-
-    return array;
-};
-
 const PasswordInput: FunctionComponent<PasswordInputProps> = ({ password, onSuccess = false }) => {
-    const passwordLength = password.length;
-    const numbersInputs = createArray(passwordLength);
-    const newArr: string[] = new Array(numbersInputs).fill(false).fill(true, 0, passwordLength);
     const result: string[] = password.split("");
+    let iterator = 0;
+    const shuffledArrayWithInputs = useCreateShuffledArrayWithInputs(password.length);
 
-    // const twoArrays = newArr.concat(result);
-    // const newArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
-    // console.log(result);
-    // console.log(newArr);
-    // console.log(twoArrays);
-    // console.log(numbersInputs);
-    const resultAfterReduce = result.reduce((prev, curr, index) => {
-        console.log(prev[index]);
-        prev[index] = curr;
+    const filledArrayWithPassword = shuffledArrayWithInputs.reduce((prev, curr, index) => {
+        if (prev[index]) {
+            prev[index] = result[iterator];
+            iterator++;
+        }
         return prev;
-    }, newArr);
-    // console.log(resultAfterReduce);
-    // console.log(resultAfterReduce);
-    // shuffle(resultAfterReduce);
-
-    // NOTE: przypisany_index jest losowy w przedziale: minimalny 0 lub każdy kolejny, maksymalny to losowa liczba newArr.lenght - result.lenght
-    // newArr[przypisany_index] = result[0];
+    }, shuffledArrayWithInputs);
 
     return (
         <div className="password-input">
