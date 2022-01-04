b0c7a83a01982d7c8623b74e8ed72e2860d8ee11
Krystian Stopka <kr6stopka@gmail.com>
2022-01-04 18:10:08 +0100

    validation yup

--- src/index.tsx
+++ src/index.tsx
@@ -26,12 +26,12 @@ import reportWebVitals from "./reportWebVitals";
 // import App from "./react_TS/4_MasonryGrid/App";
 // import App from "./react_TS/5_Modal/App";
 // import App from "./react_TS/6_SearchWithDropdown/App";
-import App from "./react_TS/7_TableWithPagination/App";
+// import App from "./react_TS/7_TableWithPagination/App";
 // import App from "./react_TS/8_InfiniteHumanList/App";
 // import App from "./react_TS/8_InfiniteHumanList_v2/App";
 // import App from "./react_TS/9_PasswordInput/App";
 // import App from "./react_TS/10_MultistepForm_v1/App";
-// import App from "./react_TS/10_MultistepForm_v2/App";
+import App from "./react_TS/10_MultistepForm_v2/App";
 
 ReactDOM.render(
     <React.StrictMode>
