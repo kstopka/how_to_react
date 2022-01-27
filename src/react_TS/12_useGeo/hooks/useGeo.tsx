48943aa31cfe3d19b7ff413e6794dfa411667eef
Krystian Stopka <kr6stopka@gmail.com>
2022-01-27 15:18:19 +0100

    useGeo Watching, test useGeo

--- src/react_TS/12_useGeo/hooks/useGeo.tsx
+++ src/react_TS/12_useGeo/hooks/useGeo.tsx
@@ -1,24 +1,29 @@
-import { useReducer } from "react";
+import { useReducer, useRef } from "react";
 import { geoDataReducer, geoDataInitialState } from "../reducer/geoDataReducer";
 import { ActionType, geoDataType } from "../App.d";
 
 export const useGeo = (): [geoDataType, () => void] => {
     const [{ isToggle, geoData }, geoDataDispatch] = useReducer(geoDataReducer, geoDataInitialState);
+    const locationWatchId = useRef(0);
+
+    const success = ({ coords }: GeolocationPosition) => {
+        const { latitude, longitude } = coords;
+        geoDataDispatch({ type: ActionType.changeLocation, latitude, longitude });
+        if (locationWatchId.current && navigator.geolocation) {
+            navigator.geolocation.clearWatch(locationWatchId.current);
+        }
+    };
 
     const toggleListening = (): void => {
-        const success = ({ coords }: GeolocationPosition) => {
-            const { latitude, longitude } = coords;
-            geoDataDispatch({ type: ActionType.changeLocation, latitude, longitude });
-        };
-
         if (isToggle) {
             geoDataDispatch({ type: ActionType.toggleListeningLocation, isToggle: false });
+            // navigator.geolocation.getCurrentPosition(success);
+
             //watching przy przemieszczaniu sie
-            navigator.geolocation.getCurrentPosition(success);
+            locationWatchId.current = navigator.geolocation.watchPosition(success);
         } else {
             geoDataDispatch({ type: ActionType.resetLocation });
         }
     };
-
     return [geoData, toggleListening];
 };
