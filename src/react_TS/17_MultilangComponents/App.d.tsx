c115007198cb3644969379d8c757a62a68931e63
Krystian Stopka <kr6stopka@gmail.com>
2022-07-01 14:19:07 +0200

    context training

--- src/react_TS/17_MultilangComponents/App.d.tsx
+++ src/react_TS/17_MultilangComponents/App.d.tsx
@@ -13,3 +13,9 @@ export type ContextType = {
     state: LangInitialState;
     dispatch: React.Dispatch<any>;
 };
+
+export enum Types {
+    reset = "reset",
+    addition = "addition",
+    subtraction = "subtraction",
+}
